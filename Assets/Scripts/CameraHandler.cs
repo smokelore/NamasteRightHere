4ae186c5589374f9ddc1bf1016b7dd917c1c9439
Sebastian Monroy <sebastian.monroy@gmail.com>
2015-03-27 15:31:13 -0400

    basic gyro slerping

--- Assets/Scripts/CameraHandler.cs
+++ Assets/Scripts/CameraHandler.cs
@@ -4,13 +4,26 @@ using System.Collections.Generic;
 
 public class CameraHandler : MonoBehaviour
 {
+	public GameObject Sphere;
+
 	void Start ()
 	{
-	
+		Input.gyro.enabled = true;
 	}
 	
 	void Update ()
 	{
-	
+		UpdatePosition();
+		UpdateRotation();
+	}
+
+	void UpdatePosition()
+	{
+		this.transform.position = Input.gyro.attitude.eulerAngles.normalized * 2f;
+	}
+
+	void UpdateRotation()
+	{
+		this.transform.LookAt(Sphere.transform.position);
 	}
 }
--- Assets/Scripts/SphereHandler.cs
+++ Assets/Scripts/SphereHandler.cs
@@ -4,13 +4,32 @@ using System.Collections.Generic;
 
 public class SphereHandler : MonoBehaviour
 {
-	void Start ()
+	public static SphereHandler instance;
+	public GameObject Handle;
+	public GameObject Target;
+
+	void Awake ()
 	{
-	
+		instance = this;
 	}
 	
 	void Update ()
 	{
-	
+		Quaternion newRot = Quaternion.Inverse(Input.gyro.attitude);
+		newRot = Quaternion.Slerp(this.transform.rotation, newRot, 5 * Time.deltaTime);
+		this.transform.rotation = newRot;
+	}
+
+	public void NextTargetPosition()
+	{
+		Target.transform.position = CalculateNewTargetPosition();
+	}
+
+	public Vector3 CalculateNewTargetPosition()
+	{
+		// TEMPORARY HACK, MAKE DEPENDENT ON PREVIOUS POSITIONS TOO
+		Vector3 newPos = Random.onUnitSphere;
+
+		return newPos;
 	}
 }
